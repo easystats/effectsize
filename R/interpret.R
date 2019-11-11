c5bbcf3992818c9949419ab443584cbc45142116
Dominique Makowski <dom.mak19@gmail.com>
2019-11-11 22:18:53 +0800

    simplify rules creation and add closest
    close #13

--- R/interpret.R
+++ R/interpret.R
@@ -1,31 +1,54 @@
+# Rules ---------------------------------------------------------------
+
+
 #' Interpretation Grid
 #'
-#' Create a container for interpretation rules of thumb. See \link{interpret}.
+#' Create a container for interpretation rules of thumb. Usually used in conjunction with \link{interpret}.
 #'
-#' @param breakpoints Vector of value break points (edges defining categories).
-#' @param labels Labels associated with each category. Must contain one label more than breakpoints.
-#' @param if_lower If true, each label will be given if the value is strictly lower than its breakpoint. The contrary if false.
-#'
+#' @param values Vector of reference values (edges defining categories or critical values).
+#' @param labels Labels associated with each category. If \code{NULL}, will try to infer it from \code{values} (if it is a named vector or a list), otherwise, will return the breakpoints.
 #'
 #'
+#' @seealso interpret
 #'
 #' @examples
-#' rules(c(0.05), c("significant", "not significant"), if_lower = TRUE)
+#' rules(c(0.05), c("significant", "not significant"))
+#' rules(c(0.2, 0.5, 0.8), c("small", "medium", "large"))
+#' rules(c("small" = 0.2, "medium" = 0.5))
 #' @export
-rules <- function(breakpoints, labels, if_lower = TRUE) {
-  if (length(breakpoints) != length(labels) - 1) {
-    stop("There must be exactly one more label than breakpoints.")
-  } else if (is.unsorted(breakpoints)) {
-    stop("Breakpoints must be sorted.")
+rules <- function(values, labels = NULL) {
+
+  if(is.null(labels)){
+    if(is.list(values)){
+      values <- unlist(values)
+    }
+    if(is.null(names(values))){
+      labels <- values
+    } else{
+      labels <- names(values)
+    }
   }
 
+  # Sanity checks
+  if(length(labels) < length(values)){
+    stop("There cannot be less labels than reference values!")
+  } else if(length(labels) > length(values) + 1){
+    stop("Too many labels for the number of reference values!")
+  }
+
+  if (length(values) == length(labels) - 1) {
+    if (is.unsorted(values)){
+      stop("Reference values must be sorted.")
+    }
+  }
+
+  # Store and return
   out <- list(
-    breakpoints = breakpoints,
-    labels = labels,
-    if_lower = if_lower
+    values = values,
+    labels = labels
   )
   class(out) <- c("rules", "list")
-  return(out)
+  out
 }
 
 
@@ -37,6 +60,34 @@ rules <- function(breakpoints, labels, if_lower = TRUE) {
 is.rules <- function(x) inherits(x, "rules")
 
 
+
+#' @importFrom utils head
+#' @export
+print.rules <- function(x, ...){
+
+  if(length(x$values) == length(x$labels)){
+    insight::print_color("# Reference values\n\n", "blue")
+    df <- data.frame(t(rep(NA, length(x$labels))))
+    names(df) <- x$labels
+    df[1, ] <- x$values
+    cat(insight::format_table(df))
+  } else{
+    insight::print_color("# Reference thresholds\n\n", "blue")
+    cat(paste0(head(x$labels, -1), " < ", x$values, " < ", x$labels[-1], collapse = " < "))
+  }
+}
+
+
+
+
+
+
+
+
+# Interpret ---------------------------------------------------------------
+
+
+
 #' Generic function for interpretation
 #'
 #' Interpret a value based on a set of rules. See \link{rules}.
@@ -44,15 +95,23 @@ is.rules <- function(x) inherits(x, "rules")
 #' @param x Vector of value break points (edges defining categories).
 #' @param rules Set of \link{rules}.
 #'
-#'
+#' @seealso rules
 #' @examples
 #' rules_grid <- rules(c(0.01, 0.05), c("very significant", "significant", "not significant"))
 #' interpret(0.001, rules_grid)
 #' interpret(0.021, rules_grid)
 #' interpret(0.08, rules_grid)
 #' interpret(c(0.01, 0.005, 0.08), rules_grid)
+#'
+#' interpret(c(0.35, 0.15), c("small" = 0.2, "large" = 0.4))
+#' interpret(c(0.35, 0.15), rules(c(0.2, 0.4), c("small", "medium", "large")))
 #' @export
 interpret <- function(x, rules) {
+
+  if(!inherits(rules, "rules")){
+    rules <- rules(rules)
+  }
+
   if (length(x) > 1) {
     return(sapply(x, .interpret, rules))
   } else {
@@ -65,21 +124,15 @@ interpret <- function(x, rules) {
 
 #' @keywords internal
 .interpret <- function(x, rules) {
-  if (rules$if_lower == TRUE) {
-    check <- x < rules$breakpoints
-    if (TRUE %in% check) {
-      index <- min(which(check))
-    } else {
-      index <- length(rules$labels)
-    }
-  } else {
-    check <- x <= rules$breakpoints
+  if(length(rules$values) == length(rules$labels)){
+    index <- which.min(abs(x - rules$values))
+  } else{
+    check <- x < rules$values
     if (TRUE %in% check) {
       index <- min(which(check))
     } else {
       index <- length(rules$labels)
     }
   }
-  out <- rules$labels[index]
-  return(out)
+  rules$labels[index]
 }
