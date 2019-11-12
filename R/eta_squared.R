7df9164d03145824b074f6647edebc92289ade33
mattansb <35330040+mattansb@users.noreply.github.com>
2019-11-12 10:27:53 +0200

    polish eta_squared
    #5

--- R/eta_squared.R
+++ R/eta_squared.R
@@ -87,7 +87,22 @@ eta_squared.lm <- eta_squared.aov
 eta_squared.glm <- eta_squared.aov
 
 #' @export
-eta_squared.anova <- eta_squared.aov
+eta_squared.anova <- function(model, partial = TRUE, ci = NULL, iterations = 1000, ...) {
+  if ("DenDF" %in% colnames(model)) {
+    if (isFALSE(partial)) {
+      warning("Currently only supports partial eta squared for mixed models.")
+    }
+    par_table <- as.data.frame(model)
+    par_table$Parameter <- rownames(par_table)
+    colnames(par_table)[colnames(par_table) == "NumDF"] <- "df"
+    colnames(par_table)[colnames(par_table) == "DenDF"] <- "df2"
+    colnames(par_table)[colnames(par_table) == "F value"] <- "F"
+
+    .eta_square_from_F(par_table, ci = ci)
+  } else {
+    eta_squared.aov(model, partial = partial, ci = ci, iterations = iterations, ...)
+  }
+}
 
 
 #' @export
@@ -112,20 +127,14 @@ eta_squared.aovlist <- function(model, partial = TRUE, ci = NULL, ...) {
 
 #' @export
 eta_squared.merMod <- function(model, partial = TRUE, ci = NULL, ...) {
-  if (isFALSE(partial)) {
-    warning("Currently only supports partial eta squared for moxed models.")
-  }
-
   if (!requireNamespace("lmerTest", quietly = TRUE)) {
     stop("Package 'lmerTest' required for this function to work. Please install it by running `install.packages('lmerTest')`.")
   }
 
 
   model <- lmerTest::as_lmerModLmerTest(model)
-  par_table <- stats::anova(model)
-  par_table <- cbind(Parameter = rownames(par_table), par_table)
-  colnames(par_table)[4:6] <- c("df", "df2", "F")
-  .eta_square_from_F(par_table, ci = ci)
+  model <- stats::anova(model)
+  eta_squared.anova(model, partial = partial, ci = ci, ...)
 }
 
 
@@ -183,8 +192,7 @@ eta_squared.merMod <- function(model, partial = TRUE, ci = NULL, ...) {
 
 #' @keywords internal
 .eta_square_from_F <- function(.data, ci = NULL) {
-  .data$Eta_Sq_partial <- .data$`F` * .data$df /
-    (.data$`F` * .data$df + .data$df2)
+  .data$Eta_Sq_partial <- F_to_eta2(.data$`F`, .data$df, .data$df2)
 
   if (is.numeric(ci)) {
     .data$CI_high <- .data$CI_low <- NA
