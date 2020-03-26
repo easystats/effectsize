597c87657f3f518765d639263140f8799baa15d4
mattansb <35330040+mattansb@users.noreply.github.com>
2020-03-26 08:10:01 +0200

    fix test

--- tests/testthat/test_standardized_differences.R
+++ tests/testthat/test_standardized_differences.R
@@ -2,6 +2,6 @@ if (require("testthat") && require("effectsize")) {
   test_that("cohens_d", {
     rez_t <- t.test(iris$Sepal.Length, iris$Sepal.Width)
     rez_d <- cohens_d(iris$Sepal.Length, iris$Sepal.Width)
-    testthat::expect_true(sign(rez_t$statistic) == sign(rez_d))
+    testthat::expect_true(sign(rez_t$statistic) == sign(rez_d$Cohens_d))
   })
 }
