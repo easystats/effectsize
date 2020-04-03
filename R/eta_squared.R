7dcc54b3f31d5605c130c189c155d28cc292c554
Daniel <mail@danielluedecke.de>
2020-04-03 15:52:25 +0200

    close #61

--- R/eta_squared.R
+++ R/eta_squared.R
@@ -303,38 +303,57 @@ cohens_f <- function(model, partial = TRUE, ci = 0.9, ...) {
                                        ...) {
   type <- match.arg(type)
 
-  if ("t" %in% colnames(model)) {
-    f <- model[["t"]]^2
-  }
-
-  if ("F" %in% colnames(model)) {
-    f <- model[["F"]]
-  }
-
-  if ("z" %in% colnames(model)) {
-    stop("Cannot compute effect size from models with no proper residual variance. Consider the estimates themselves as indices of effect size.")
-  }
-
-  df_col <- colnames(model)[colnames(model) %in% c("df", "Df", "NumDF")]
-  if (length(df_col)) {
-    df_num <- model[[df_col]][!is.na(f)]
+  if ("Group" %in% colnames(model)) {
+    x <- split(model, model$Group)
+    out <- do.call(rbind, lapply(x, function(i) {
+      f <- i[["F"]]
+      df_num <- i[["df"]][!is.na(f)]
+      df_error <- i[i$Parameter == "Residuals", "df"]
+      cbind(
+        data.frame(Group = unique(i$Group), stringsAsFactors = FALSE),
+        .anova_es_model_params(i, f, df_num, df_error, type, ci)
+      )
+    }))
   } else {
-    df_num <- 1
+    if ("t" %in% colnames(model)) {
+      f <- model[["t"]]^2
+    }
+    if ("F" %in% colnames(model)) {
+      f <- model[["F"]]
+    }
+    if ("z" %in% colnames(model)) {
+      stop("Cannot compute effect size from models with no proper residual variance. Consider the estimates themselves as indices of effect size.")
+    }
+
+    df_col <- colnames(model)[colnames(model) %in% c("df", "Df", "NumDF")]
+    if (length(df_col)) {
+      df_num <- model[[df_col]][!is.na(f)]
+    } else {
+      df_num <- 1
+    }
+
+    if ("df_error" %in% colnames(model)) {
+      df_error <- model$df_error
+    } else if ("Residuals" %in% model$Parameter) {
+      df_error <- model[model$Parameter == "Residuals", df_col]
+    } else {
+      stop("Cannot extract degrees of freedom for the error term. Try passing the model object directly to 'eta_squared()'.")
+    }
+    out <- .anova_es_model_params(model, f, df_num, df_error, type, ci)
   }
 
+  class(out) <- c("effectsize_table", "data.frame")
+  out
+}
 
-  if ("df_error" %in% colnames(model)) {
-    df_error <- model$df_error
-  } else if ("Residuals" %in% model$Parameter) {
-    df_error <- model[model$Parameter == "Residuals", df_col]
-  } else {
-    stop("Cannot extract degrees of freedom for the error term. Try passing the model object directly to 'eta_squared()'.")
-  }
 
+.anova_es_model_params <- function(model, f, df_num, df_error, type, ci) {
   out <- .F_to_pve(stats::na.omit(f), df = df_num, df_error = df_error, ci = ci, es = paste0(type, "2"))
   out$Parameter <- model$Parameter[!is.na(f)]
-  out[c(ncol(out), 1:4)]
+  out[c(ncol(out), 1:(ncol(out) - 1))]
 }
+
+
 
 
 #' @keywords internal
