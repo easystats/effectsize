5d7d3e8ed4947af84224971006f5ee1b207d8123
mattansb <35330040+mattansb@users.noreply.github.com>
2020-04-29 08:54:49 +0300

    equitest and std_params test

--- tests/testthat/test-standardize_parameters.R
+++ tests/testthat/test-standardize_parameters.R
@@ -7,4 +7,81 @@ if (require("testthat") && require("effectsize")) {
     es <- standardize_parameters(model)[2, 2]
     testthat::expect_equal(es, r, tol = 0.01)
   })
+
+  test_that("standardize_parameters (lm with ci)", {
+    model <- lm(Sepal.Length ~ Species + Petal.Width, data = iris)
+
+    testthat::expect_equal(
+      standardize_parameters(model, method = "refit")$Std_Coefficient,
+      c(0.044, -0.072, -0.060, 0.844),
+      tol = 0.01
+    )
+
+    testthat::expect_equal(
+      standardize_parameters(model, method = "posthoc")$Std_Coefficient,
+      c(0, -0.072, -0.060, 0.844),
+      tol = 0.01
+    )
+
+    testthat::expect_equal(
+      standardize_parameters(model, method = "smart")$Std_Coefficient,
+      c(0, -0.170, -0.142, 0.844),
+      tol = 0.01
+    )
+
+    testthat::expect_equal(
+      standardize_parameters(model, method = "basic")$Std_Coefficient,
+      c(0, -0.034, -0.028, 0.844),
+      tol = 0.01
+    )
+
+    ## CI
+    testthat::expect_equal(
+      standardize_parameters(model, method = "basic")$CI_low,
+      c(0, -0.294, -0.433, 0.491),
+      tol = 0.01
+    )
+
+    testthat::expect_equal(
+      standardize_parameters(model, method = "basic")$CI_high,
+      c(0, 0.225, 0.375, 1.196),
+      tol = 0.01
+    )
+
+    testthat::expect_equal(
+      standardize_parameters(model, ci = 0.8, method = "basic")$CI_low,
+      c(0, -0.203, -0.292, 0.614),
+      tol = 0.01
+    )
+
+    testthat::expect_equal(
+      standardize_parameters(model, ci = 0.8, method = "basic")$CI_high,
+      c(0, 0.135, 0.234, 1.073),
+      tol = 0.01
+    )
+  })
+
+  if (require(rstanarm)) {
+    test_that("standardize_parameters (simple)", {
+      testthat::skip_on_cran()
+      set.seed(1234)
+      suppressWarnings(
+        model <- stan_glm(Sepal.Length ~ Species + Petal.Width,
+                          data = iris,
+                          iter = 500, refresh = 0)
+      )
+
+      testthat::expect_equal(
+        suppressWarnings(standardize_parameters(model, method = "refit")$Std_Median),
+        c(0.037, -0.061, -0.060, 0.838),
+        tol = 0.01
+      )
+
+      testthat::expect_equal(
+        suppressWarnings(standardize_parameters(model, method = "posthoc")$Std_Median),
+        c(NA, -0.050, -0.030,  0.833),
+        tol = 0.01
+      )
+    })
+  }
 }
