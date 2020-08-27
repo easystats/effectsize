082f91a70eea00e9556d1f80fbc52570a40b117f
Daniel <mail@danielluedecke.de>
2020-08-27 17:22:28 +0200

    catch log-log

--- R/standardize.models.R
+++ R/standardize.models.R
@@ -168,7 +168,7 @@ standardize.coxme <- standardize.coxph
 .log_terms <- function(model, data) {
   x <- insight::find_terms(model, flatten = TRUE)
   # log_pattern <- "^log\\((.*)\\)"
-  log_pattern <- "(log|log1|log10|log1p|log2)\\(([^,)]*).*"
+  log_pattern <- "(log\\(log|log|log1|log10|log1p|log2)\\(([^,)]*).*"
   out <- gsub(log_pattern, "\\2", x[grepl(log_pattern, x)])
   intersect(colnames(data), out)
 }
