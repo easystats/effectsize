607ff22edc79863a38ae24688f4128ead391de07
mattansb <35330040+mattansb@users.noreply.github.com>
2020-09-15 15:38:36 +0300

    Update print.effectsize_difference.R

--- R/print.effectsize_difference.R
+++ R/print.effectsize_difference.R
@@ -7,8 +7,8 @@ print.effectsize_difference <- function(x, digits = 2, append_CL = FALSE, ...) {
   x_orig <- x
   print.effectsize_table(x, digits = digits)
 
-  if (append_CL && ("Cohens_d" %in% colnames(x) || "Hedges_g" %in% colnames(x))) {
-    cl <- d_to_common_language(x$Cohens_d)
+  if (append_CL && any(colnames(x) %in% c("Cohens_d", "Hedges_g"))) {
+    cl <- d_to_common_language(x[[any(colnames(x) %in% c("Cohens_d", "Hedges_g"))]])
     cl <- sapply(cl, insight::format_value, digits = digits, as_percent = TRUE)
     cl <- paste(paste0("* ", names(cl),": ",cl), collapse = "\n")
     cat("\n")
