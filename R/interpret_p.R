e69c6c73475dbaa5d6c3e7326f3fd721173e37d8
mattansb <35330040+mattansb@users.noreply.github.com>
2020-09-20 15:31:52 +0300

    Clean `interpert_*` rules #131

--- R/interpret_p.R
+++ R/interpret_p.R
@@ -1,9 +1,11 @@
 #' Interpret p-values
 #'
 #' @param p Value or vector of p-values.
-#' @param rules Can be `"default"` or custom set of [rules()].
+#' @param rules Can be `"default"`, `"rss"` (for *Redefine statistical
+#'   significance* rules) or custom set of [rules()].
 #'
-#'
+#' @references
+#' - Benjamin, D. J., Berger, J. O., Johannesson, M., Nosek, B. A., Wagenmakers, E. J., Berk, R., ... & Cesarini, D. (2018). Redefine statistical significance. Nature Human Behaviour, 2(1), 6-10.
 #'
 #' @examples
 #' interpret_p(.02)
@@ -16,8 +18,10 @@ interpret_p <- function(p, rules = "default") {
   } else {
     if (rules == "default") {
       return(interpret(p, rules(c(0.05), c("significant", "not significant"))))
+    } else if (rules == "rss") {
+      return(interpret(p, rules(c(0.005, 0.05), c("significant", "suggestive", "not significant"))))
     } else {
-      stop("rules must be 'default' or an object of type rules.")
+      stop("rules must be 'default', 'rss' or an object of type rules.")
     }
   }
 }
