a62dbf680ff8ea1cc4b756f706370c649c7b9383
mattansb <35330040+mattansb@users.noreply.github.com>
2020-09-28 16:43:36 +0300

    merge dataset docs

--- R/datasets.R
+++ R/datasets.R
@@ -1,7 +1,21 @@
+#' Workers' salary and other information
+#'
+#' A sample (simulated) dataset, used in tests and some examples.
+#'
 #' @docType data
 #' @title Sample data set
+#'
 #' @name hardlyworking
+#'
 #' @keywords data
 #'
-#' @description A sample data set, used in tests and some examples.
+#' @format A data frame with 500 rows and 5 variables:
+#' \describe{
+#'   \item{salary}{Salary, in Shmekels}
+#'   \item{xtra_hours}{Number of overtime hours (on average, per week)}
+#'   \item{n_comps}{Number of compliments given to the boss (observed over the last week)}
+#'   \item{age}{Age in years}
+#'   \item{seniority}{How many years with the company}
+#' }
+#'
 NULL
