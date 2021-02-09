df7b5c2bcb610ce02eafd109ff9d704b5b64b382
DominiqueMakowski <dom.mak19@gmail.com>
2021-02-09 08:41:41 +0800

    added lovakov2021 rules of thumb
    Aka improvements of the best functions of effectsize :grin:
    
    @mattansb that said I couldn't access the full article https://onlinelibrary.wiley.com/doi/10.1002/ejsp.2752 (it's too soon for sci-hub it seems), but if you manage to get it can you forward it to me so we can make sure that the labels are correct (for now I reused the same labels as cohen's, as it is what is suggested in the abstract)

--- R/interpret_d.R
+++ R/interpret_d.R
@@ -4,12 +4,12 @@
 #'
 #'
 #' @param d,g,delta Value or vector of effect size values.
-#' @param rules Can be `"cohen1988"` (default), `"gignac2016"`, `"sawilowsky2009"` or custom set of [rules()].
+#' @param rules Can be `"cohen1988"` (default), `"gignac2016"`, `"sawilowsky2009"`, `"lovakov2021"` or a custom set of [rules()].
 #' @param ... Not directly used.
 #'
 #' @section Rules:
 #'
-#' Rules apply to equally to positive and negative *d*.
+#' Rules apply to equally to positive and negative *d* (i.e., they are given as absolute values).
 #'
 #' - Cohen (1988) (`"cohen1988"`; default)
 #'   - **d < 0.2** - Very small
@@ -24,6 +24,11 @@
 #'   - **0.8 <= d < 1.2** - Large
 #'   - **1.2 <= d < 2** - Very large
 #'   - **d >= 2** - Huge
+#' - Lovakov & Agadullina (2021) (`"lovakov2021"`)
+#'   - **d < 0.15** - Very small
+#'   - **0.15 <= d < 0.36** - Small
+#'   - **0.36 <= d < 0.65** - Medium
+#'   - **d >= 0.65** - Large
 #' - Gignac & Szodorai (2016) (`"gignac2016"`, based on the [d_to_r()] conversion, see [interpret_r()])
 #'   - **d < 0.2** - Very small
 #'   - **0.2 <= d < 0.41** - Small
@@ -33,7 +38,9 @@
 #' @examples
 #' interpret_d(.02)
 #' interpret_d(c(.5, .02))
+#' interpret_d(.3, rules = "lovakov2021")
 #' @references
+#' - Lovakov, A., & Agadullina, E. R. (2021). Empirically Derived Guidelines for Effect Size Interpretation in Social Psychology. European Journal of Social Psychology.
 #' - Gignac, G. E., & Szodorai, E. T. (2016). Effect size guidelines for individual differences researchers. Personality and individual differences, 102, 74-78.
 #' - Cohen, J. (1988). Statistical power analysis for the behavioral sciences (2nd Ed.). New York: Routledge.
 #' - Sawilowsky, S. S. (2009). New effect size rules of thumb.
@@ -53,6 +60,10 @@ interpret_d <- function(d, rules = "cohen1988", ...) {
       sawilowsky2009 = rules(c(0.1, 0.2, 0.5, 0.8, 1.2, 2),
         c("tiny", "very small", "small", "medium", "large", "very large", "huge"),
         name = "sawilowsky2009", right = FALSE
+      ),
+      lovakov2021 = rules(c(0.15, 0.36, 0.65),
+        c("very small", "small", "medium", "large"),
+        name = "lovakov2021", right = FALSE
       ),
       gignac2016 = NA # added for the correct error msg
     )
--- R/interpret_r.R
+++ R/interpret_r.R
@@ -1,7 +1,7 @@
 #' Interpret correlation
 #'
 #' @param r Value or vector of correlation coefficient.
-#' @param rules Can be `"funder2019"` (default), `"gignac2016"`, `"cohen1988"`, `"evans1996"` or custom set of [rules()].
+#' @param rules Can be `"funder2019"` (default), `"gignac2016"`, `"cohen1988"`, `"evans1996"`, `"lovakov2021"` or a custom set of [rules()].
 #'
 #' @section Rules:
 #'
@@ -24,6 +24,11 @@
 #'   - **0.1 <= r < 0.3** - Small
 #'   - **0.3 <= r < 0.5** - Moderate
 #'   - **r >= 0.5** - Large
+#' - Lovakov & Agadullina (2021) (`"lovakov2021"`)
+#'   - **r < 0.1** - Very small
+#'   - **0.1 <= r < 0.3** - Small
+#'   - **0.3 <= r < 0.5** - Moderate
+#'   - **r >= 0.5** - Large
 #' - Evans (1996) (`"evans1996"`)
 #'   - **r < 0.2** - Very weak
 #'   - **0.2 <= r < 0.4** - Weak
@@ -34,9 +39,11 @@
 #' @examples
 #' interpret_r(.015)
 #' interpret_r(c(.5, -.02))
+#' interpret_r(.3, rules = "lovakov2021")
 #' @seealso Page 88 of APA's 6th Edition.
 #'
 #' @references
+#' - Lovakov, A., & Agadullina, E. R. (2021). Empirically Derived Guidelines for Effect Size Interpretation in Social Psychology. European Journal of Social Psychology.
 #' - Funder, D. C., & Ozer, D. J. (2019). Evaluating effect size in psychological research: sense and nonsense. Advances in Methods and Practices in Psychological Science.
 #' - Gignac, G. E., & Szodorai, E. T. (2016). Effect size guidelines for individual differences researchers. Personality and individual differences, 102, 74-78.
 #' - Cohen, J. (1988). Statistical power analysis for the behavioral sciences (2nd Ed.). New York: Routledge.
@@ -62,6 +69,10 @@ interpret_r <- function(r, rules = "funder2019") {
       evans1996 = rules(c(0.2, 0.4, 0.6, 0.8),
         c("very weak", "weak", "moderate", "strong", "very strong"),
         name = "evans1996", right = FALSE
+      ),
+      lovakov2021 = rules(c(0.12, 0.24, 0.41),
+        c("very small", "small", "medium", "large"),
+        name = "lovakov2021", right = FALSE
       )
     )
   )
