a8e9150418c2b24231e6ad46f0e48b3608426fc8
mattansb <35330040+mattansb@users.noreply.github.com>
2021-02-15 08:51:03 +0200

    format_standardize now actually returns "Mean" label

--- tests/testthat/test-format_standardize.R
+++ tests/testthat/test-format_standardize.R
@@ -6,34 +6,26 @@ if (require("testthat") && require("effectsize")) {
       structure(3:1, .Label = c("+1 SD", "Mean", "-1 SD"), class = "factor")
     )
 
-    set.seed(123)
-    mod1 <- format_standardize(c(-1, 0, 1, 2), reference = rnorm(1000))
+
+    skip_if_not_installed("bayestestR")
+    ref <- bayestestR::distribution_normal(1000)
 
     expect_equal(
-      mod1,
-      structure(4:1, .Label = c(
-        "+2 SD", "+1e+00 SD", "-2e-02 SD",
-        "-1 SD"
-      ), class = "factor")
+      format_standardize(c(-1, 0, 1, 2), reference = ref),
+      structure(4:1, .Label = c("+2 SD", "+1 SD", "Mean", "-1 SD"),
+                class = "factor")
     )
 
-    set.seed(123)
-    mod2 <- format_standardize(c(-1, 0, 1, 2), reference = rnorm(1000), robust = TRUE)
-
     expect_equal(
-      mod2,
-      structure(4:1, .Label = c("+2 MAD", "+1 MAD", "-1e-02 MAD", "-1 MAD"), class = "factor")
+      format_standardize(c(-1, 0, 1, 2), reference = ref, robust = TRUE),
+      structure(4:1, .Label = c("+2 MAD", "+1 MAD", "Median", "-1 MAD"),
+                class = "factor")
     )
 
-    set.seed(123)
-    mod3 <- format_standardize(c(-1, 0, 1, 2), reference = rnorm(1000), robust = TRUE, digits = 2)
-
     expect_equal(
-      mod3,
-      structure(4:1, .Label = c(
-        "+2.06 MAD", "+1.03 MAD", "-9.55e-03 MAD",
-        "-1.05 MAD"
-      ), class = "factor")
+      format_standardize(c(-1, 0, 1, 2), reference = ref, robust = TRUE, digits = 2),
+      structure(4:1, .Label = c("+2.00 MAD", "+1.00 MAD", "+0.00 MAD", "-1.00 MAD"),
+                class = "factor")
     )
   })
 }
