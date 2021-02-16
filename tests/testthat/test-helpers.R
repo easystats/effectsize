b96fa97be2e3b454b67a6323621cd850ffe09fe6
mattansb <35330040+mattansb@users.noreply.github.com>
2021-02-16 11:13:35 +0200

    remove some tests

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,10 +1,4 @@
 if (require("testthat") && require("effectsize")) {
-  test_that(".factor_to_numeric works", {
-    expect_equal(.factor_to_numeric(as.factor(c(1, 2, 3, 4))), c(1, 2, 3, 4))
-    expect_equal(.factor_to_numeric(as.factor(c(1, 2, NA, 4))), c(1, 2, NA, 4))
-    expect_equal(.factor_to_numeric(c(1, 2, 3, 4)), c(1, 2, 3, 4))
-  })
-
   test_that("is_effectsize_name works", {
     expect_false(is_effectsize_name("is_effectsize_name"))
     expect_true(is_effectsize_name("Eta2"))
