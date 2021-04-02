493c6dcb68063d817fd60502ecf0a826d95011d9
mattansb <35330040+mattansb@users.noreply.github.com>
2021-04-02 11:14:39 +0300

    move tests

--- tests/testthat/test-eta_squared_etc.R
+++ tests/testthat/test-eta_squared_etc.R
@@ -399,4 +399,41 @@ if (require("testthat") && require("effectsize")) {
     expect_equal(resE1[[2]][1], 0.9386555, tolerance = 0.01)
     expect_equal(resE1[[2]][1], resA1[[5]][1], tolerance = 0.01)
   })
+
+
+  # tidymodels --------------------------------------------------------------
+  test_that("ets_squared | tidymodels", {
+    skip_if_not_installed("tidymodels")
+    require("tidymodels", quietly = TRUE)
+
+    set.seed(123)
+    mod_lm <-
+      linear_reg() %>%
+      set_engine("lm") %>%
+      set_mode("regression") %>%
+      fit(mpg ~ am + vs, data = mtcars)
+
+    set.seed(123)
+    df_lm <- as.data.frame(eta_squared(mod_lm, ci = 0.95))
+
+    expect_equal(
+      df_lm,
+      structure(
+        list(
+          Parameter = c("am", "vs"),
+          Eta2_partial = c(0.534050980382337, 0.509657899929074),
+          CI = c(0.95, 0.95),
+          CI_low = c(0.270477566572333, 0.242410781810851),
+          CI_high = c(0.698019260414593, 0.681154789342689)
+        ),
+        class = "data.frame",
+        row.names = 1:2,
+        partial = TRUE,
+        generalized = FALSE,
+        ci = 0.95,
+        ci_method = list(method = "ncp", distribution = "t")
+      ),
+      tolerance = 0.001
+    )
+  })
 }
