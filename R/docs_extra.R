04a618d2c721ade7d805d7fd7d25432839e66d6b
Indrajeet Patil <inderonline1988@gmail.com>
2021-05-10 16:27:31 +0200

    bump easystats dep versions; style and spelling

--- R/docs_extra.R
+++ R/docs_extra.R
@@ -25,7 +25,7 @@
 #' associated with Chi-squared and F distributions), this applies also to cases
 #' where the lower bound of the CI is equal to 0. Even more care should be taken
 #' when the *upper* bound is equal to 0 - this occurs when *p*-value is greater
-#' than 1−alpha/2 making, the upper bound unestimatable, and the upper bound is
+#' than 1−alpha/2 making, the upper bound inestimable, and the upper bound is
 #' arbitrarily sets to 0 (Steiger, 2004). For example:
 #' ```{r}
 #' eta_squared(aov(mpg ~ factor(gear) + factor(cyl), mtcars[1:7, ]))
--- tests/testthat/test-sd_pooled.R
+++ tests/testthat/test-sd_pooled.R
@@ -1,9 +1,9 @@
 if (require("testthat") && require("effectsize")) {
   test_that("sd_pooled", {
-    expect_equal(sd_pooled(1:4, 1:3*5), 3.316625, tolerance = 0.001)
-    expect_equal(mad_pooled(1:4, 1:3*5), 3.297154, tolerance = 0.001)
+    expect_equal(sd_pooled(1:4, 1:3 * 5), 3.316625, tolerance = 0.001)
+    expect_equal(mad_pooled(1:4, 1:3 * 5), 3.297154, tolerance = 0.001)
 
-    expect_equal(sd_pooled(c(1:3,40), 1:3*5), 15.06652, tolerance = 0.001)
-    expect_equal(mad_pooled(c(1:3,40), 1:3*5), 3.297154, tolerance = 0.001)
+    expect_equal(sd_pooled(c(1:3, 40), 1:3 * 5), 15.06652, tolerance = 0.001)
+    expect_equal(mad_pooled(c(1:3, 40), 1:3 * 5), 3.297154, tolerance = 0.001)
   })
 }
