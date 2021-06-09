20fd24c8bb4a57c2d6f36bae5a0e5c26bd49d097
Dominique Makowski <dom.mak19@gmail.com>
2021-06-09 08:52:08 +0800

    add interpret_pd, that for some reason was missing

---
+++ R/interpret_pd.R
@@ -0,0 +1,40 @@
+#' Interpret Probability of Direction (pd)
+#'
+#' @param x Value or vector of probabilities of direction (pd).
+#'
+#' @section Rules:
+#'
+#' - Default (i.e., equivalent to p-values)
+#'   - **pd <= 0.975** - not significant
+#'   - **pd > 0.975** - significant
+#'
+#' - Makowski et al. (2019) (`"makowski2019"`)
+#'   - **pd <= 0.95** - uncertain
+#'   - **pd > 0.95** - possibly existing
+#'   - **pd > 0.97** - likely existing
+#'   - **pd > 0.99** - probably existing
+#'   - **pd > 0.999** - certainly existing
+#'
+#' @examples
+#' interpret_pd(.98)
+#' interpret_pd(c(.96, .99), rules = "makowski2019")
+#'
+#' @references
+#' - Makowski, D., Ben-Shachar, M. S., Chen, S. H., \& Lüdecke, D. (2019). Indices of effect existence and significance in the Bayesian framework. Frontiers in psychology, 10, 2767.
+#'
+#' @export
+interpret_pd <- function(pd, rules = "default") {
+  rules <- .match.rules(
+    rules,
+    list(
+      default = rules(c(0.975), c("not significant", "significant"),
+                      name = "default", right = FALSE
+      ),
+      makowski2019 = rules(c(0.95, 0.97, 0.99, 0.999), c("uncertain", "possibly existing", "likely existing", "probably existing", "certainly existing"),
+                  name = "makowski2019", right = FALSE
+      )
+    )
+  )
+
+  interpret(pd, rules)
+}
