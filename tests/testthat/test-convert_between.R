be9f2598862d58392fe697ed15e64b51aab9186d
mattansb <35330040+mattansb@users.noreply.github.com>
2021-09-10 15:11:52 +0300

    OR_to_RR for glmms

--- tests/testthat/test-convert_between.R
+++ tests/testthat/test-convert_between.R
@@ -29,6 +29,7 @@ if (require("testthat") && require("effectsize")) {
     expect_equal(riskratio_to_oddsratio(log(RR), p0 = p0, log = TRUE), log(OR))
     expect_equal(oddsratio_to_riskratio(log(OR), p0 = p0, log = TRUE), log(RR))
 
+    # -- GLMs --
 
     m <- glm(am ~ factor(cyl), data = mtcars,
              family = binomial())
@@ -48,6 +49,20 @@ if (require("testthat") && require("effectsize")) {
     expect_false("(Intercept)" %in% RR$Parameter)
     # these values confirmed from emmeans
     expect_equal(RR$Coefficient, c(0.05, 0.29173, 0.06557), tolerance = 0.001)
+
+    # -- GLMMs --
+    skip_if_not_installed("lme4")
+    m <- lme4::glmer(am ~ factor(cyl) + (1|gear), data = mtcars,
+                     family = binomial())
+    w <- capture_warnings(RR <- oddsratio_to_riskratio(m))
+    expect_match(w[1],"p0")
+    expect_match(w[2],"CIs")
+    expect_true("(Intercept)" %in% RR$Parameter)
+    expect_false("(p0)" %in% RR$Parameter)
+    # these values confirmed from emmeans
+    expect_equal(RR$Coefficient, c(0.7048, 0.6042, 0.4475), tolerance = 0.001)
+    expect_equal(RR$CI_low, c(NA, 0.08556, 0.0102), tolerance = 0.001)
+    expect_equal(RR$CI_high, c(NA, 1.2706, 1.3718), tolerance = 0.001)
   })
 
   test_that("odds_to_probs", {
