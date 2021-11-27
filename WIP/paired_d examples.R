e0a7c3a564de998a961882d27b8cfb9e8b25be84
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2021-11-27 21:15:11 +0200

    even more #372

--- WIP/paired_d examples.R
+++ WIP/paired_d examples.R
@@ -2,48 +2,39 @@ library(effectsize)
 
 dat <- read.table("http://pcl.missouri.edu/exp/effectSizePuzzler.txt", header=TRUE)
 
-paired_d(rt ~ cond | id, data = dat, type = "z") # 1.353713 DONE
-
+paired_d(rt ~ cond | id, data = dat, type = "z") # 1.353713
 paired_d(rt ~ cond | id, data = dat, type = "d") # 0.2497971
 paired_d(rt ~ cond | id, data = dat, type = "a") # 0.8357347
 # paired_d(rt ~ cond | id, data = dat, type = "t") # withOUT sqrt(2) bias, is the same as d_z
 paired_d(rt ~ cond | id, data = dat, type = "r") # 0.259
 
-head(dat)
 
-# https://doi.org/10.3389/fpsyg.2013.00863
-dat_m <- tapply(dat$rt, list(dat$id, dat$cond), mean)
-rr <- cor(dat_m)[2]
-MM <- apply(dat_m, 2, mean)
-SS <- apply(dat_m, 2, sd)
-
-# Cohen's d RM 0.267
-sqrt(2 * (1-rr)) * diff(MM) /
-  sqrt(sum(SS ^ 2) + 2 * rr * prod(SS))
-
-#Cohen's d AV 0.209
-diff(MM) / sqrt(sum(SS) / 2)
+# Test
+paired_d(rt ~ cond | id, data = dat, type = "d", mu = 0.05)
+paired_d(rt ~ cond | id, data = dat, type = "d", alternative = "l")
 
 
-# For bootstarp?
-data <- data.frame(
-  x = dat$rt,
-  groups = dat$cond |> factor(),
-  blocks = dat$id |> factor()
-)
+# More types ----
+data(stroop, package = "afex")
+stroop <- subset(stroop, study == 1 & condition == "control" & acc == 1)
+stroop <- subset(stroop, pno %in% levels(pno)[1:10])
 
-sample_rm_data <- function(data) {
-  tmp_data <- split(data, data$blocks)
-  tmp_data <- sample(tmp_data, replace = TRUE)
-  tmp_data <- mapply(tmp_data, seq_along(tmp_data), FUN = function(d, id) {
-    d$blocks <- id
-    split(d, d$groups) <- lapply(split(d, d$groups), function(.d) .d[sample(nrow(.d), replace = TRUE), ])
-    d
-  }, SIMPLIFY = FALSE)
-  do.call("rbind", tmp_data)
-}
+paired_d(rt ~ congruency | pno, data = stroop, type = "z", ci = NULL)
+paired_d(rt ~ congruency | pno, data = stroop, type = "d", ci = NULL)
+paired_d(rt ~ congruency | pno, data = stroop, type = "a", ci = NULL)
+paired_d(rt ~ congruency | pno, data = stroop, type = "r", ci = NULL)
 
+data_agg <- aggregate(stroop$rt, stroop[, c("pno", "congruency")], mean)
+d <- diff(tapply(data_agg$x, data_agg$congruency, mean))
+ss <- tapply(data_agg$x, data_agg$congruency, sd)
 
-sample_rm_data(data) |> head()
+# type av (close to type a)
+s <- mean(ss)
+d / s
 
-
+# type rm (close to type r?)
+data_agg <- data_agg[order(data_agg$pno),]
+data_agg <- split(data_agg, data_agg$congruency)
+r <- cor(sapply(data_agg, "[[", "x"))[1,2]
+s <- sqrt(sum(ss^2) - 2 * r * prod(ss))
+(d / s) * sqrt(2 * (1-r))
--- WIP/paired_d.R
+++ WIP/paired_d.R
@@ -1,7 +1,10 @@
 paired_d <- function(x, group, block, data = NULL,
                      mu = 0, ci = 0.95, alternative = "two.sided",
-                     type = c("d", "z", "a", "r")) {
+                     iterations = 200,
+                     type = c("d", "a", "r", "z")) {
   type <- match.arg(type)
+  alternative <- match.arg(alternative, c("two.sided", "less", "greater"))
+
   data <- effectsize:::.kendalls_w_data(x, group, block, data, wide = FALSE)
   data$groups <- factor(data$groups)
   data$blocks <- factor(data$blocks)
@@ -16,12 +19,7 @@ paired_d <- function(x, group, block, data = NULL,
     if (type == "a") type <- "d"
   }
 
-  if (type == "d") {
-    x <- split(data$x, data$groups)
-    names(x) <- c("x", "y")
-    d <- unname(diff(sapply(x, mean)))
-    s <- do.call(sd_pooled, x)
-  } else if (type == "z") {
+  if (type == "z") {
     data <- data[order(data$groups),]
     data <- aggregate(data$x, data[2], function(.x) {
       if (length(.x) != 2L){
@@ -31,41 +29,43 @@ paired_d <- function(x, group, block, data = NULL,
       }
     })
     return(cohens_d(data$x, mu = mu, ci = ci, alternative = alternative))
-    # d <- mean(data$x, na.rm = TRUE)
-    # s <- sd(data$x, na.rm = TRUE)
-  } else if (type == "r") {
-    # insight::check_if_installed("lme4")
-    # mod <- tryCatch({
-    #   lme4::lmer(x ~ groups + (groups | blocks), data = data)
-    # }, error = function(...) {
-    #   lme4::lmer(x ~ groups + (1 | blocks), data = data)
-    # })
-    # d <- unname(lme4::fixef(mod)[2])
-    # s <- sigma(mod)
-
-    # Use ANOVA decomp instead
-    mod <- aov(x ~ groups + Error(blocks), data = data) # blocks/groups? TryCatch?
-    summ <- summary(mod)
-    s <- sqrt(summ[["Error: Within"]][[1]][2,"Mean Sq"])
-    browser()
   }
 
+  insight::check_if_installed("lme4")
+  mod <- tryCatch({
+    lme4::lmer(x ~ groups + (groups | blocks), data = data)
+  }, error = function(...) {
+    lme4::lmer(x ~ groups + (1 | blocks), data = data)
+  })
 
-  out <- data.frame(d = (d - mu) / s)
+  # # Use ANOVA decomp instead?
+  # mod <- aov(x ~ groups + Error(blocks), data = data) # blocks/groups? TryCatch?
+  # summ <- summary(mod)
+  # d <- coef(mod[["Within"]])
+  # s <- sqrt(summ[["Error: Within"]][[1]][2,"Mean Sq"])
+
+  out <- data.frame(d = .d_from_lmer(mod, type = type, mu = mu))
   if (orig_type != "d") names(out) <- paste0("d_", orig_type)
 
-  if (!is.null(ci)) {
+  if (!is.null(ci) &&
+      insight::check_if_installed("boot", stop = FALSE, reason = "for CI estimation")) {
     stopifnot(length(ci) == 1, ci < 1, ci > 0)
 
-    # Add cis
-    # USE emmeans::eff_size???? (w/ qdrg?)
+
     out$CI <- ci
     ci.level <- if (alternative == "two.sided") ci else 2 * ci - 1
 
-    out$CI_low <- -Inf
-    out$CI_high <- Inf
+    sims <- lme4::bootMer(
+      mod, nsim = iterations, use.u = TRUE,
+      FUN = function(.m) .d_from_lmer(.m, type = type, mu = mu)
+    )
 
-    ci_method <- list()
+    CIs <- boot::boot.ci(sims, type = "perc", conf = ci.level)$percent[,4:5]
+
+    out$CI_low <- CIs[1]
+    out$CI_high <- CIs[2]
+
+    ci_method <- list(method = "percentile bootstrap", iterations = iterations)
     if (alternative == "less") {
       out$CI_low <- -Inf
     } else if (alternative == "greater") {
@@ -86,4 +86,15 @@ paired_d <- function(x, group, block, data = NULL,
 }
 
 
+.d_from_lmer <- function(mod, type, mu) {
+  d <- unname(lme4::fixef(mod)[2])
 
+  if (type == "d") {
+    s <- sqrt(insight::get_variance_intercept(mod) +
+                insight::get_variance_residual(mod, verbose = FALSE))
+  } else if (type == "r") {
+    s <- sigma(mod)
+  }
+
+  unname((d - mu) / s)
+}
