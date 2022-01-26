c395b2d31150c94017a15c5e9ac0c9ba47640da9
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2022-01-26 16:32:35 +0200

    v bump

--- R/reexports.R
+++ R/reexports.R
@@ -5,29 +5,3 @@ bayestestR::equivalence_test
 #' @export
 #' @importFrom datawizard standardize
 datawizard::standardize
-
-
-
-
-
-## TODO: removed the following from see / modelbased --------------------
-
-#' @export
-#' @importFrom datawizard ranktransform
-datawizard::ranktransform
-
-#' @export
-#' @importFrom datawizard adjust
-datawizard::adjust
-
-#' @export
-#' @importFrom datawizard change_scale
-datawizard::change_scale
-
-#' @export
-#' @importFrom datawizard normalize
-datawizard::normalize
-
-#' @export
-#' @importFrom datawizard unstandardize
-datawizard::unstandardize
