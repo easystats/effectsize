68366c582e95ab77281dfb646c2e523ec2a462f4
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2022-01-28 00:25:31 +0200

    only 7 method of paired_d
    #372

--- WIP/paired_d examples.R
+++ WIP/paired_d examples.R
@@ -1,42 +1,23 @@
 library(effectsize)
 
+source("WIP/paired_d2.R")
+
 dat <- read.table("http://pcl.missouri.edu/exp/effectSizePuzzler.txt", header=TRUE)
 
-paired_d(rt ~ cond | id, data = dat, type = "z") # 1.353713
+# From http://jakewestfall.org/blog/index.php/2016/03/25/five-different-cohens-d-statistics-for-within-subject-designs/
 paired_d(rt ~ cond | id, data = dat, type = "d") # 0.2497971
 paired_d(rt ~ cond | id, data = dat, type = "a") # 0.8357347
-# paired_d(rt ~ cond | id, data = dat, type = "t") # withOUT sqrt(2) bias, is the same as d_z
+paired_d(rt ~ cond | id, data = dat, type = "z") # 1.353713
+paired_d(rt ~ cond | id, data = dat, type = "t") # 1.914439
 paired_d(rt ~ cond | id, data = dat, type = "r") # 0.259
 
+# More types:
+# https://doi.org/10.3389/fpsyg.2013.00863
+# https://journals.sagepub.com/doi/pdf/10.1177/0013164403256358 (CIs)
+paired_d(rt ~ cond | id, data = dat, type = "rm")
+paired_d(rt ~ cond | id, data = dat, type = "av")
 
-# Test
+
+# Test ----
 paired_d(rt ~ cond | id, data = dat, type = "d", mu = 0.05)
 paired_d(rt ~ cond | id, data = dat, type = "d", alternative = "l")
-
-
-# More types ----
-data(stroop, package = "afex")
-stroop <- subset(stroop, study == 1 & condition == "control" & acc == 1)
-stroop <- subset(stroop, pno %in% levels(pno)[1:10])
-
-paired_d(rt ~ congruency | pno, data = stroop, type = "z", ci = NULL)
-paired_d(rt ~ congruency | pno, data = stroop, type = "d", ci = NULL)
-paired_d(rt ~ congruency | pno, data = stroop, type = "a", ci = NULL)
-paired_d(rt ~ congruency | pno, data = stroop, type = "r", ci = NULL)
-
-data_agg <- aggregate(stroop$rt, stroop[, c("pno", "congruency")], mean)
-d <- diff(tapply(data_agg$x, data_agg$congruency, mean))
-ss <- tapply(data_agg$x, data_agg$congruency, sd)
-
-# type av (close to type a)
-s <- mean(ss)
-d / s
-
-# type rm (close to type r?) NOPE
-data_agg <- data_agg[order(data_agg$pno),]
-data_agg <- split(data_agg, data_agg$congruency)
-r <- cor(sapply(data_agg, "[[", "x"))[1,2]
-s <- sqrt(sum(ss^2) - 2 * r * prod(ss))
-(d / s) * sqrt(2 * (1-r))
-
-effsize::cohen.d(data_agg[[1]]$x, data_agg[[2]]$x, paired = TRUE)
---
+++ WIP/paired_d2.R
@@ -0,0 +1,154 @@
+paired_d <- function(x, group, block, data = NULL,
+                     mu = 0, ci = 0.95, alternative = "two.sided",
+                     type = c("d", "a", "r", "z", "t", "av", "rm")) {
+  type <- match.arg(type)
+  alternative <- match.arg(alternative, c("two.sided", "less", "greater"))
+
+  data <- effectsize:::.kendalls_w_data(x, group, block, data, wide = FALSE)
+  data$groups <- factor(data$groups)
+  data$blocks <- factor(data$blocks)
+
+  stopifnot(nlevels(data$groups) == 2L)
+
+  if (type %in% c("a", "z", "t", "av", "rm")) {
+    data <- aggregate(data$x, data[-1], mean)
+  }
+
+  if (type %in% c("z", "t")) {
+    out <- paired_d_z_t(data, type, mu = mu, ci = ci, alternative = alternative)
+  } else if (type %in% c("av", "rm")) {
+    out <- paired_d_av_rm(data, type, mu = mu, ci = ci, alternative = alternative)
+  } else if (type %in% c("d", "a", "r")) {
+    out <- paired_d_d_a_r(data, type, mu = mu, ci = ci, alternative = alternative)
+  }
+
+  attr(out, "paired_type") <- type
+  out
+}
+
+paired_d_z_t <- function(data, type,
+                         mu = 0, ci = 0.95, alternative = "two.sided") {
+  data <- data[order(data$groups),]
+  data <- aggregate(data$x, data[2], function(.x) {
+    if (length(.x) != 2L) return(NA)
+    diff(.x)
+  })
+
+  out <- cohens_d(data$x, mu = mu, ci = ci, alternative = alternative)
+  colnames(out)[1] <- "d_z"
+
+  if (type == "t") {
+    colnames(out)[1] <- "d_t"
+    out[colnames(out) %in% c("d_t", "CI_low", "CI_high")] <-
+      sqrt(2) * out[colnames(out) %in% c("d_t", "CI_low", "CI_high")]
+  }
+
+  out
+}
+
+paired_d_d_a_r <- function(data, type,
+                         mu = 0, ci = 0.95, alternative = "two.sided") {
+  # Use ANOVA decomp
+  mod <- stats::aov(x ~ groups + Error(blocks/groups), data = data)
+  pars <- parameters::model_parameters(mod)
+
+  is_d <- (pars$Paramete %in% c("groups", "blocks:groups") & pars$df==1)
+  if (sum(is_d)>1) is_d <- is_d & pars$Group == "blocks:groups"
+  d <- unname(coef(mod[[pars$Group[is_d]]]))
+
+  if (type %in% c("d", "a")) {
+    ss <- sum(pars$Sum_Squares[!is_d])
+    df <- sum(pars$df[!is_d])
+    s <- sqrt(ss/df)
+  } else if (type == "r") {
+    is_r <- pars$Group == "Within" & pars$Parameter == "Residuals"
+    ss <- pars$Sum_Squares[is_r]
+    df <- pars$df[is_r]
+    s <- sqrt(ss/df)
+  }
+
+  out <- data.frame(d = (d - mu)/s)
+  if (type != "d") names(out) <- paste0("d_", type)
+
+  if (!is.null(ci)) {
+    stopifnot(length(ci) == 1, ci < 1, ci > 0)
+
+    out$CI <- ci
+    ci.level <- if (alternative == "two.sided") ci else 2 * ci - 1
+
+    ssd <- ((d-mu)/2)^2*insight::n_obs(mod)
+    t <- sign(d) * sqrt(ssd / (ss / df))
+    ts <- effectsize:::.get_ncp_t(t, df, ci.level)
+
+    out$CI_low <- 2 * ts[1] / sqrt(df)
+    out$CI_high <- 2 * ts[2] / sqrt(df)
+    ci_method <- list(method = "ncp", distribution = "t")
+    if (alternative == "less") {
+      out$CI_low <- -Inf
+    } else if (alternative == "greater") {
+      out$CI_high <- Inf
+    }
+  } else {
+    ci_method <- alternative <- NULL
+  }
+
+  class(out) <- c("effectsize_difference", "effectsize_table", "see_effectsize_table", class(out))
+  attr(out, "mu") <- mu
+  attr(out, "ci") <- ci
+  attr(out, "ci_method") <- ci_method
+  attr(out, "approximate") <- FALSE
+  attr(out, "alternative") <- alternative
+  return(out)
+}
+
+paired_d_av_rm <- function(data, type,
+                           mu = 0, ci = 0.95, alternative = "two.sided") {
+  data <- data[order(data[[1]], data[[2]]),]
+
+  d <- diff(tapply(data$x, data$groups, mean))
+  ss <- tapply(data$x, data$groups, sd)
+
+  r <- cor(data$x[data$groups==data$groups[1]],
+           data$x[data$groups!=data$groups[1]])
+  co <- cov(data$x[data$groups==data$groups[1]],
+            data$x[data$groups!=data$groups[1]])
+  n <- nrow(data)/2
+
+  Sdiff <- sqrt(sum(ss^2) -2*co)
+
+  if (type == "av") {
+    out <- data.frame(d_av = (d - mu) / mean(ss))
+  } else if (type == "rm") {
+    out <- data.frame(d_rm = sqrt(2 * (1 - r)) * (d - mu) / Sdiff)
+  }
+
+
+  if (!is.null(ci)) {
+    stopifnot(length(ci) == 1, ci < 1, ci > 0)
+
+    out$CI <- ci
+    ci.level <- if (alternative == "two.sided") ci else 2 * ci - 1
+
+    tc <- qt(0.5+ci.level/2, df = n-1, lower.tail = TRUE)
+    se <- sqrt((2*Sdiff^2)/(n * sum(ss)))
+
+    out$CI_low <- out[[1]] - tc * se
+    out$CI_high <- out[[1]] + tc * se
+    ci_method <- list(method = "normal")
+    if (alternative == "less") {
+      out$CI_low <- -Inf
+    } else if (alternative == "greater") {
+      out$CI_high <- Inf
+    }
+  } else {
+    ci_method <- alternative <- NULL
+  }
+
+  class(out) <- c("effectsize_difference", "effectsize_table", "see_effectsize_table", class(out))
+  attr(out, "mu") <- mu
+  attr(out, "ci") <- ci
+  attr(out, "ci_method") <- ci_method
+  attr(out, "approximate") <- FALSE
+  attr(out, "alternative") <- alternative
+  return(out)
+}
