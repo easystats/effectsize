fc154a912fcd64efbd8087302db7cc74465bd34f
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2022-02-17 09:33:56 +0200

    Update test-plot.R

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -1,7 +1,7 @@
-if (require(effectsize) && require(testthat) &&
-    require(see) && require(ggplot2)) {
+if (require(effectsize) && require(testthat)) {
   test_that("plot methods", {
     skip_if_not_installed("see", "0.6.8")
+    skip_if_not_installed("ggplot2")
     expect_error(plot(d <- cohens_d(mpg ~ am, data = mtcars)), NA)
     expect_s3_class(plot(d), "ggplot")
 
