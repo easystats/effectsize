c2f8e890a705347f5b400ef9f8758d96c9f17439
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2022-02-22 13:37:43 +0200

    print md/html for rules
    who needs this? no idea...

--- R/print.rules.R
+++ R/print.rules.R
@@ -5,18 +5,43 @@ print.rules <- function(x, digits = "signif2", ...) {
   if (length(x$values) == length(x$labels)) {
     cat(insight::export_table(x_fmt, align = "rl", format = NULL, sep = " ~ ", ...))
   } else {
-    cat(insight::export_table(x_fmt, align = "rcccl", format = NULL, sep = " ", ...))
+    cat(insight::export_table(x_fmt, align = "rcl", format = NULL, sep = " ", ...))
   }
   invisible(x)
 }
 
 
-#' @importFrom utils head tail
 #' @export
-format.rules <- function(x, ...) {
+print_md.rules <- function(x, digits = "signif2", ...) {
+  x_fmt <- format(x, digits = digits, output = "markdown", ...)
+
+  if (length(x$values) == length(x$labels)) {
+    insight::export_table(x_fmt, align = "rl", format = "markdown", ...)
+  } else {
+    insight::export_table(x_fmt, align = "rcl", format = "markdown", ...)
+  }
+}
+
+
+#' @export
+print_html.rules <- function(x, digits = "signif2", ...) {
+  x_fmt <- format(x, digits = digits, output = "html", ...)
+
+  if (length(x$values) == length(x$labels)) {
+    insight::export_table(x_fmt, align = "rl", format = "html", ...)
+  } else {
+    insight::export_table(x_fmt, align = "rcl", format = "html", ...)
+  }
+}
+
+
+
+#' @export
+format.rules <- function(x, digits = "signif2", output = "text", ...) {
   name <- attr(x, "rule_name")
 
   V <- insight::format_value(x$values, ...)
+  V <- insight::format_value(V, width = max(nchar(V)), ...)
   L <- x$labels
 
   if (length(V) == length(L)) {
@@ -30,25 +55,31 @@ format.rules <- function(x, ...) {
     title_type <- "Thresholds"
 
     if (isTRUE(attr(x, "right"))) {
-      gLeft <- " < "
-      gRight <- " <= "
+      gLeft <- " <"
+      gRight <- "<= "
     } else {
-      gLeft <- " <= "
-      gRight <- " < "
+      gLeft <- " <="
+      gRight <- "< "
     }
 
     out <- data.frame(
-      " " = c(NA, V),
-      " " = c(NA, rep(gLeft, length(V))),
+      Lower = paste0(c("", V), c("", rep(gLeft, length(V)))),
       Label = L,
-      " " = c(rep(gRight, length(V)), NA),
-      " " = c(V, NA),
+      Upper = paste0(c(rep(gRight, length(V)), ""), c(V, "")),
       check.names = FALSE
     )
   }
 
-  attr(out, "table_caption") <-
-    c(sprintf("# Reference %s (%s)\n\n", title_type, name), .pcl["interpret"])
+  if (output == "text") {
+    colnames(out)[c(1,3)] <- " "
+    caption <- c(sprintf("# Reference %s (%s)", title_type, name),
+                 .pcl["interpret"])
+  } else {
+    caption <- sprintf("Reference %s (%s)", title_type, name)
+  }
+
+  attr(out, "table_caption") <- caption
+
   out
 }
 
