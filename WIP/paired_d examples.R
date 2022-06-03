6cde8ae59bb759655a1852837fd82d0f6b0a8ebb
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2022-06-03 20:59:21 +0300

    Update paired_d examples.R

--- WIP/paired_d examples.R
+++ WIP/paired_d examples.R
@@ -2,43 +2,52 @@ library(effectsize)
 
 source("WIP/paired_d2.R")
 
-dat <- read.table("http://pcl.missouri.edu/exp/effectSizePuzzler.txt", header=TRUE)
-
-# From http://jakewestfall.org/blog/index.php/2016/03/25/five-different-cohens-d-statistics-for-within-subject-designs/
-# More types:
-# https://doi.org/10.3389/fpsyg.2013.00863
-# https://journals.sagepub.com/doi/pdf/10.1177/0013164403256358 (CIs)
-
-# Are CIs correct?
-# Need to simulate data with a big subject:condition interaction.
-
-#' Single measurement:
-#' d - classic definition
-#' r - standardize by the residual sd (sigma)
-paired_d(rt ~ cond | id, data = dat, type = "d") # 0.2497971
-paired_d(rt ~ cond | id, data = dat, type = "r") # 0.2587388
-
-#' Aggregate:
-#' a - aggregate
-#' z - standardized scores
-#' t - (incorrect) conversion from t statistic (d_z * sqrt(2))
-#' rm - d, but accounts for correlation between repeated measures (not a good name)
-#' av - d, but just use average sd (isn't this type a?)
-paired_d(rt ~ cond | id, data = dat, type = "a") # 0.8357347
-paired_d(rt ~ cond | id, data = dat, type = "z") # 1.353713
-paired_d(rt ~ cond | id, data = dat, type = "t") # 1.914439
-paired_d(rt ~ cond | id, data = dat, type = "rm")
-paired_d(rt ~ cond | id, data = dat, type = "av")
+#' dat <- read.table("http://pcl.missouri.edu/exp/effectSizePuzzler.txt", header=TRUE)
+#'
+#' # From http://jakewestfall.org/blog/index.php/2016/03/25/five-different-cohens-d-statistics-for-within-subject-designs/
+#' # More types:
+#' # https://doi.org/10.3389/fpsyg.2013.00863
+#' # https://journals.sagepub.com/doi/pdf/10.1177/0013164403256358 (CIs)
+#'
+#' # Are CIs correct?
+#' # Need to simulate data with a big subject:condition interaction.
+#'
+#' #' Single measurement:
+#' #' d - classic definition
+#' #' r - standardize by the residual sd (sigma)
+#' paired_d(rt ~ cond | id, data = dat, type = "d") # 0.2497971
+#' paired_d(rt ~ cond | id, data = dat, type = "r") # 0.2587388
+#'
+#' #' Aggregate:
+#' #' a - aggregate
+#' #' z - standardized scores
+#' #' t - (incorrect) conversion from t statistic (d_z * sqrt(2))
+#' #' rm - d, but accounts for correlation between repeated measures (not a good name)
+#' #' av - d, but just use average sd (isn't this type a?)
+#' paired_d(rt ~ cond | id, data = dat, type = "a") # 0.8357347
+#' paired_d(rt ~ cond | id, data = dat, type = "z") # 1.353713
+#' paired_d(rt ~ cond | id, data = dat, type = "t") # 1.914439
+#' paired_d(rt ~ cond | id, data = dat, type = "rm")
+#' paired_d(rt ~ cond | id, data = dat, type = "av")
 
 
 
 # Test on other data:
+library(emmeans)
 data(stroop, package = "afex")
 stroop <- subset(stroop, study == "1" & condition == "control")
+
+m <- lme4::lmer(rt ~ congruency + (congruency | pno), data = stroop)
 
 cohens_d(rt ~ congruency, data = stroop)
 paired_d(rt ~ congruency | pno, data = stroop, type = "d")
 paired_d(rt ~ congruency | pno, data = stroop, type = "r")
+
+emmeans(m, ~ congruency) |>
+  eff_size(sigma(m), df.residual(m))
+
+emmeans(m, ~ congruency) |>
+  eff_size(sd(predict(m, re.form = NA) - na.omit(stroop$rt)), df.residual(m))
 
 paired_d(rt ~ congruency | pno, data = stroop, type = "a")
 paired_d(rt ~ congruency | pno, data = stroop, type = "z")
