ddb52f3fa751c7294836084f0b5f47717d1cc23c
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2022-09-12 17:02:53 +0300

    add tests

--- tests/testthat/test-mahalanobis_D.R
+++ tests/testthat/test-mahalanobis_D.R
@@ -2,22 +2,27 @@ library(testthat)
 
 
 test_that("mahalanobis_D", {
-  skip()
+  set.seed(456)
   # should be equal
-  S <- diag(c(1, 30))
-  x <- MASS::mvrnorm(100, c(0, 0), S, empirical = TRUE) |> as.data.frame()
-  y <- MASS::mvrnorm(100, c(0, 15), S, empirical = TRUE) |> as.data.frame()
-  mahalanobis_D(x, y)
-  cohens_d(-x$V2, -y$V2)
+  x <- data.frame(A = bayestestR::distribution_normal(1000, sd = 2),
+                  B = sample(bayestestR::distribution_normal(1000, sd = 33)),
+                  C = sample(bayestestR::distribution_normal(1000, sd = 50)),
+                  C = sample(bayestestR::distribution_normal(1000, sd = 17)))
+  y <- within(x, {B <- B + 15})
+
+  D <- mahalanobis_D(x, y)
+  d <- cohens_d(-x$B, -y$B)
+  expect_equal(D[[1]], d[[1]], tolerance = 0.01)
+  expect_equal(D[[3]], d[[3]], tolerance = 0.1)
+  expect_equal(D[[4]], d[[4]], tolerance = 0.1)
+
 
   # should be equal
-  S[1,2] <- S[2,1] <- 3
-  x <- MASS::mvrnorm(100, c(0, 0), S, empirical = TRUE) |> as.data.frame()
-  y <- MASS::mvrnorm(100, c(0, 15), S, empirical = TRUE) |> as.data.frame()
-  d <- colMeans(x) - colMeans(y)
-  sqrt(t(d) %*% solve(cov_pooled(x, y)) %*% d)
-  mahalanobis_D(x, y)
+  x2 <- within(x, {B <- scale(3*A + B) * 30})
+  d <- colMeans(y) - colMeans(x2)
+  expect_equal(c(sqrt(t(d) %*% solve(cov_pooled(x2, y)) %*% d)),
+               mahalanobis_D(x2, y, ci = NULL)[[1]], tolerance = 0.01)
 
   # test input types
+  # TODO
 })
-
