24af051524cdea39fce5f7410480e3eb8d221379
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2022-09-18 15:20:45 +0300

    Update print.effectsize_table.R

--- R/print.effectsize_table.R
+++ R/print.effectsize_table.R
@@ -119,34 +119,34 @@ print.effectsize_difference <- function(x, digits = 2, append_CLES = NULL, ...) 
   print.effectsize_table(x, digits = digits, ...)
 
   if (is.logical(append_CLES) || is.character(append_CLES)) {
-    if (isTRUE(append_CLES)) {
-      append_CLES <- c("p_superiority", "u1", "u2", "u3", "overlap")
-    }
-
-    if (colnames(x_orig)[1] == "r_rank_biserial") {
-      append_CLES <- setdiff(append_CLES, "u1")
-    } else if (colnames(x_orig)[1] %in% c("Cohens_d", "Hedges_g")) {
-      if (!isTRUE(attr(x_orig, "pooled_sd"))) {
-        stop("CLES only applicable to pooled-sd Cohen's d / Hedge's g.")
-      }
-    } else {
-      stop("CLES not applicable for this effect size.")
-    }
-
     if (isTRUE(attr(x_orig, "paired"))) {
       stop("CLES only applicable to independent samples.")
     }
 
+    if (colnames(x_orig)[1] == "r_rank_biserial") {
+      cles_tab <- rb_to_p_superiority(x_orig)
+    } else if (colnames(x_orig)[1] %in% c("Cohens_d", "Hedges_g")) {
+      if (!isTRUE(attr(x_orig, "pooled_sd"))) {
+        stop("CLES only applicable to pooled-sd Cohen's d / Hedge's g.")
+      }
 
-    foos <- lapply(paste0("d_to_", append_CLES), match.fun)
-    cles <- lapply(foos, function(f) f(x_orig))
-    names(cles) <- sapply(cles, function(x) colnames(x)[1])
-    cles <- lapply(cles, function(x) {colnames(x)[1] <- "CLES"; x})
+      if (isTRUE(append_CLES)) {
+        append_CLES <- c("p_superiority", "u1", "u2", "u3", "overlap")
+      }
 
-    cles_tab <- do.call(rbind, cles)
-    cles_tab$Name <- get_effectsize_label(names(cles))
+      foos <- lapply(paste0("d_to_", append_CLES), match.fun)
+      cles <- lapply(foos, function(f) f(x_orig))
+      names(cles) <- sapply(cles, function(x) colnames(x)[1])
+      cles <- lapply(cles, function(x) {colnames(x)[1] <- "CLES"; x})
 
-    cles_tab <- cles_tab[c(ncol(cles_tab), seq_len(ncol(cles_tab)-1))]
+      cles_tab <- do.call(rbind, cles)
+      cles_tab$Name <- get_effectsize_label(names(cles))
+
+      cles_tab <- cles_tab[c(ncol(cles_tab), seq_len(ncol(cles_tab)-1))]
+
+    } else {
+      stop("CLES not applicable for this effect size.")
+    }
 
     insight::print_color("\n\n## Common Language Effect Sizes:\n", .pcl["subtitle"])
     print(cles_tab)
