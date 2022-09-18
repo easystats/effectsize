03d753db464dcce1e5b98aaf1b62fb1a1c814330
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2022-09-18 09:59:07 +0300

    init converters

--- R/zzz_deprecated.R
+++ R/zzz_deprecated.R
@@ -9,6 +9,7 @@
 #' - `interpret_parameters` for *standardized parameters* was incorrect. Use [`interpret_r`] instead.
 #' - `normalized_chi` is now [`fei`].
 #' - `chisq_to_normalized` is now [`chisq_to_fei`].
+#' - `d_to_cles` and `rb_to_cles` are now one of the available functions for CLES conversion, e.g. [`d_to_U1`].
 #'
 #' @rdname effectsize_deprecated
 #' @name effectsize_deprecated
@@ -57,4 +58,25 @@ chisq_to_normalized <- function(...) {
   chisq_to_fei(...)
 }
 
+#' @export
+d_to_cles <- function(...) {
+  .Defunct(NULL, msg = 'See help("d_to_cles") for the available functions.')
+}
 
+#' @export
+rb_to_cles <- function(...) {
+  .Deprecated("rb_to_p_superiority")
+  rb_to_p_superiority(...)
+}
+
+#' @export
+convert_d_to_common_language <- d_to_cles
+
+#' @export
+d_to_common_language <- d_to_cles
+
+#' @export
+convert_rb_to_common_language <- rb_to_cles
+
+#' @export
+rb_to_common_language <- rb_to_cles
