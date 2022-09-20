a2460dee4113b52386ec4dc226ab32da5006a291
arcaldwell49 <arcaldwell49@gmail.com>
2022-09-20 13:45:29 -0400

    For loops for simulations

--- papers/wmw_odds/R_simulation_funcs.R
+++ papers/wmw_odds/R_simulation_funcs.R
@@ -27,7 +27,43 @@ sim_2ord = function(baseprobs = c(.2,.2, .3, .2, .1),
   #return(res)
 }
 
-sim_2ord()
-res= replicate(10, sim_2ord(), simplify = FALSE)
-dplyr::bind_rows(res)
 
+
+sim_rep_2ord = function(baseprobs = c(.2,.2, .3, .2, .1),
+                        n = 30,
+                        ci = .95,
+                        odds = 1.5,
+                        nsim = 200){
+  res= replicate(nsim, sim_2ord(baseprobs = baseprobs,
+                                n = n,
+                                ci = ci,
+                                odds = odds), simplify = FALSE)
+  res_tab = dplyr::bind_rows(res)
+  res_tab$n = n
+  res_tab$odds_par = odds
+  return(res_tab)
+}
+
+
+sim_loop_2ord = function(baseprobs = c(.2,.2, .3, .2, .1),
+                         n = seq(20,50,10),
+                         ci = .95,
+                         odds = seq(0.25,1.75,.25),
+                         nsim = 200){
+
+  full_tab = data.frame()
+
+  for(ns in n){
+    for(odds_i in odds){
+      part_tab = sim_rep_2ord(baseprobs = baseprobs,
+                              n = ns,
+                              ci = ci,
+                              odds = odds_i,
+                              nsim = nsim)
+      full_tab = bind_rows(full_tab, part_tab)
+    }
+  }
+
+  return(full_tab)
+
+}
