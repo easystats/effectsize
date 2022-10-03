9786c3dad720e8c360b1191367edc476b410e363
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2022-10-03 13:05:30 +0300

    better docs for data

--- R/datasets.R
+++ R/datasets.R
@@ -22,6 +22,12 @@
 #'   \item{is_senior}{Has this person been working here for more than 4 years?}
 #' }
 #'
+#' ```{r}
+#' data("hardlyworking")
+#' head(hardlyworking, n = 5)
+#' ```
+#' @family effect size datasets
+#'
 NULL
 
 #' Results from 2 Screening Tests
@@ -40,6 +46,12 @@ NULL
 #'   \item{Test1}{Results given by the 1st test}
 #'   \item{Test2}{Results given by the 2nd test}
 #' }
+#' ```{r}
+#' data("screening_test")
+#' head(screening_test, n = 5)
+#' ```
+#'
+#' @family effect size datasets
 #'
 NULL
 
@@ -58,6 +70,13 @@ NULL
 #'
 #' @format A 2-by-2 table, with a *column* for each group and a *row* for the diagnosis.
 #'
+#' ```{r}
+#' data("RCT_table")
+#' RCT_table
+#' ```
+#'
+#' @family effect size datasets
+#'
 NULL
 
 
@@ -72,6 +91,13 @@ NULL
 #' @keywords data
 #'
 #' @format A 4-by-3 table, with a *column* for each major and a *row* for each type of music.
+#'
+#' ```{r}
+#' data("Music_preferences")
+#' Music_preferences
+#' ```
+#'
+#' @family effect size datasets
 #'
 NULL
 
@@ -88,6 +114,13 @@ NULL
 #'
 #' @format A 4-by-3 table, with a *column* for each major and a *row* for each type of music.
 #'
+#' ```{r}
+#' data("Music_preferences2")
+#' Music_preferences2
+#' ```
+#'
+#' @family effect size datasets
+#'
 NULL
 
 #' Frequency of FASD for Smoking Mothers
@@ -101,6 +134,13 @@ NULL
 #' @keywords data
 #'
 #' @format A 1-by-3 table, with a *column* for each diagnosis.
+#'
+#' ```{r}
+#' data("Smoking_FASD")
+#' Smoking_FASD
+#' ```
+#'
+#' @family effect size datasets
 #'
 NULL
 
@@ -116,5 +156,12 @@ NULL
 #' @keywords data
 #'
 #' @format A 2-by-3 table.
+#'
+#' ```{r}
+#' data("food_class")
+#' food_class
+#' ```
+#'
+#' @family effect size datasets
 #'
 NULL
