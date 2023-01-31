7067d877a425bab29268f746939cf5be9f73411a
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2023-01-31 13:44:25 +0200

    fix #560

--- R/means_ratio.R
+++ R/means_ratio.R
@@ -48,12 +48,12 @@
 #' means_ratio(3 * x, 3 * y)
 #'
 #' @references
-#' Lajeunesse, M. J. (2011). On the meta‐analysis of response ratios for studies
-#' with correlated and multi‐group designs. Ecology, 92(11), 2049-2055.
+#' Lajeunesse, M. J. (2011). On the meta-analysis of response ratios for studies
+#' with correlated and multi-group designs. Ecology, 92(11), 2049-2055.
 #' \doi{10.1890/11-0423.1}
 #'
 #' Lajeunesse, M. J. (2015). Bias and correction for the log response ratio in
-#' ecological meta‐analysis. Ecology, 96(8), 2056-2063. \doi{10.1890/14-2402.1}
+#' ecological meta-analysis. Ecology, 96(8), 2056-2063. \doi{10.1890/14-2402.1}
 #'
 #' Hedges, L. V., Gurevitch, J., & Curtis, P. S. (1999). The meta-analysis of
 #' response ratios in experimental ecology. Ecology, 80(4), 1150–1156.
