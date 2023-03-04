7495664622577a973d4af0e8dc2166b0b0eac065
Daniel <mail@danielluedecke.de>
2023-03-04 13:12:27 +0100

    styler

--- R/xtab_corr.R
+++ R/xtab_corr.R
@@ -180,7 +180,9 @@ fei <- function(x, p = rep(1, length(x)),
                 ...) {
   alternative <- .match.alt(alternative, FALSE)
   check_1d_table <- dim(x)
-  is_1d_table <- is.null(check_1d_table) || length(check_1d_table) == 1 || (length(check_1d_table) > 1 && check_1d_table[2] == 1)
+  is_1d_table <- is.null(check_1d_table) ||
+    length(check_1d_table) == 1 ||
+    (length(check_1d_table) > 1 && check_1d_table[2] == 1)
 
   if (inherits(x, "BFBayesFactor") || !is_1d_table) {
     insight::format_error("Fei is only applicable to goodness of fit tests.")
