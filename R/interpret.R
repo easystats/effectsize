6008f00df60f82ebb224577c2d4aa68b9f0963bd
Mattan S. Ben-Shachar <mattansb@msbstats.info>
2023-03-13 08:53:32 +0200

    minor clean up

--- R/interpret.R
+++ R/interpret.R
@@ -194,7 +194,7 @@ interpret.effectsize_table <- function(x, rules, ...) {
     Eta2 = ,
     Eta2_partial = ,
     Eta2_generalized = ,
-    r2_semipartial = interpret_omega_squared(value, rules = rules),
+    r2_semipartial = ,
     Epsilon2 = ,
     Epsilon2_partial = ,
     Omega2 = ,
